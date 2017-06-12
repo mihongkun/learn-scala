7513a1c06b0156063115099bea0c010426c3dc67
Jackson Mi <1045262335@qq.com>
2017-06-13 00:03:06 +0800

    性状

--- src/main/scala/DefaultGreeter.scala
+++ src/main/scala/DefaultGreeter.scala
@@ -1,6 +1,29 @@
 /**
   * Created by m on 17-6-12.
   */
-class DefaultGreeter {
+trait Greeter {
+  def greet(name: String): Unit = println("hello, " + name)
+}
 
+class DefaultGreeter extends Greeter
+
+class CustomizableGreeter(prefix: String, suffix: String) extends Greeter {
+  override def greet(name: String): Unit = {
+    println(prefix + name +  suffix)
+  }
+}
+
+
+
+
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val greeter = new DefaultGreeter()
+    greeter.greet("Scala Developer")
+
+    val customizableGreeter = new CustomizableGreeter("How are you, ","?")
+    customizableGreeter.greet("Scala Developer")
+  }
 }
